4f0b4b745ddbf993959f5bc432f6966fc92cf1c3
Alexandrodasilva <alexsamdrosilva13@gmail.com>
2021-07-14 12:21:38 -0300

    adicionando novas linhas de código para o arquivo de arvore binaria e arovore de buscas

--- activity_04/arvoreBusca.c
+++ activity_04/arvoreBusca.c
@@ -6,6 +6,7 @@ struct NO{
     struct NO *esq;
     struct NO *dir;
 };
+
 typedef struct  NO* arv;
 
 arv* criar(){
@@ -32,13 +33,6 @@ void libera_arvore(arv* r){
     }
     liberaNO(*r); 
     free(r);
-}
-
-int arvoreVazia(arv *r){
-    if(r == NULL){
-        return (1);
-    }
-    return(0);
 }
 
 void preOrdemArvBin(arv *r){   //RED
@@ -71,28 +65,60 @@ void posOrdemArvBin(arv *r){   //ERD
         printf("%d\n", (*r)->dados);
     }
 }
-int insercaoArvBusca(arv *r, int info){
-    if(r = NULL){
+int insercaoArvBusca(arv* r, int info){
+
+    if(r == NULL){
         return 0;
     }
-    struct NO* aux = (struct NO*) malloc(sizeof(struct NO));
+    struct NO* aux = (struct NO*) malloc(sizeof(struct NO)); // só para esquecer, mas o NO*  é para referenciar o tamnhanho do aux como ponteiro
     if(aux  == NULL){
         return 0;   
     }
     aux->dados =  info;
     aux->dir = NULL;
     aux->esq = NULL;
+
     if(*r == NULL){
         *r = aux;
     }
     else{
-        
+        struct NO* atual = *r;
+        struct NO* ant = NULL;
+        while (atual != NULL)
+        {
+            ant = atual;
+            if(info == atual->dados){
+                free(aux);
+                return 0;
+            }
+            if(info > atual->dados){
+                atual = atual->dir;
+            }else{
+                atual = atual->esq;
+            }
+        }
+        if(info > ant->dados){
+            ant->dir = aux;
+        }else{
+            ant->esq = aux;
+        }
+    return 1;
     }
 }
+
 
 int main(void){
     arv* r;
     r = criar();
-    printf("hello word\n");
+    insercaoArvBusca(r, 10);
+    insercaoArvBusca(r, 7);
+    insercaoArvBusca(r, 23);
+    insercaoArvBusca(r, 22);
+    insercaoArvBusca(r, 9);
+    preOrdemArvBin(r);
+   printf("hello word\n");
+   posOrdemArvBin(r);
+   printf("hello word\n");
+   OrdemArvBin(r);
     return(0);
 }
